1f580d2df88ad0416ecf40a229b759c1505c23ae
TheRealRemo <timothyprice2001@gmail.com>
2026-05-12 18:19:37 -0400

    Built 'display' method with switch case for user processing 'Vehicle' inventory search filtering (in progress)

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -5,31 +5,61 @@ import java.util.Scanner;
 
 public class UserInterface {
   private Dealership  dealership;
-  private static void ledgerMenu(Scanner scanner) {
 
+  public UserInterface() {
+  }
+  private void init(){}
+  public void display(){ Scanner scanner = new Scanner(System.in);
+    init();
     boolean running = true;
-    while (running) {
-      //sort taking and comparing first date with safety net then
-      // comparing time if multiple dates are on the same day
-      System.out.println("Ledger");
-      System.out.println("Choose an option:");
-      System.out.println("A) All");
-      System.out.println("D) Deposits");
-      System.out.println("P) Payments");
-      System.out.println("R) Reports");
-      System.out.println("H) Home");
 
+    while (running) {
+      System.out.println("\n========================");
+      System.out.println("Car Dealership Inventory");
+      System.out.println("========================");
+      System.out.println("Choose an option:");
+      System.out.println("1) Search Vehicles By Price");
+      System.out.println("2) Search Vehicles By Make & Model");
+      System.out.println("3) Search Vehicles By Year");
+      System.out.println("4) Search Vehicles By Color");
+      System.out.println("5) Search Vehicles By Mileage");
+      System.out.println("6) Search Vehicles By Vehicle Type");
+      System.out.println("7) Show All Vehicles");
+      System.out.println("8) Add Vehicle To Inventory");
+      System.out.println("9) Remove Vehicle From Inventory");
+      System.out.println("X) Exit");
+      System.out.println("========================");
+      System.out.print("Please enter here: ");
       String input = scanner.nextLine().trim();
+      if (input.equalsIgnoreCase("x")) {
+        System.out.println("Goodbye!");
+      }
 
       switch (input.toUpperCase()) {
-        case "A" -> displayLedger();
-        case "D" -> displayDeposits();
-        case "P" -> displayPayments();
-        case "R" -> reportsMenu(scanner);
-        case "H" -> running = false;
+        case "1" -> processGetByPriceRequest();
+        case "2" -> processGetByMakeModelRequest();
+        case "3" -> processGetByYearRequest();
+        case "4" -> processGetByColorRequest();
+        case "5" -> processGetByMileageRequest();
+        case "6" -> processGetByVehicleTypeRequest();
+        case "7" -> processGetAllVehiclesRequest();
+        case "8" -> processAddVehicleRequest();
+        case "9" -> processRemoveVehicleRequest();
+        case "X" -> running = false;
         default -> System.out.println("Invalid option");
       }
     }
+    scanner.close();
   }
+  public void processGetByPriceRequest(){}
+  public void processGetByMakeModelRequest(){}
+  public void processGetByYearRequest(){}
+  public void processGetByColorRequest(){}
+  public void processGetByMileageRequest(){}
+  public void processGetByVehicleTypeRequest(){}
+  public void processGetAllVehiclesRequest(){}
+  public void processAddVehicleRequest(){}
+  public void processRemoveVehicleRequest(){}
+
 
 }
